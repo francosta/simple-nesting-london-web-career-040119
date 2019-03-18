09d1d13bb4c73f03d69725e9eef8fa4b2e2ff1b5
Francisco Costa <francisco@fcosta.pt>
2019-03-18 11:21:57 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -61,7 +61,7 @@ def dennis_ritchies_language
         }
      }
 		 binding.pry
-programmer_hash[:dennis_ritchies_language][:languages]
+programmer_hash[:dennis_ritchie_language][:languages]
 end
 
 def adding_matz
