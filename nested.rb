cb45ecae09cc3d1a91c108cdf34ffc03b10d5c55
Francisco Costa <francisco@fcosta.pt>
2019-03-18 11:30:05 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -90,7 +90,7 @@ def adding_matz
 		 binding.pry
 		 programmer_hash[:yukihiro_matsumoto] =
 			 :known_for => "Ruby",
-			 :languages => "LISP", "C"
+			 :languages => ["LISP", "C"]
 
 		 programmer_hash
 
